bbdbd100fbec9fbb250dafeffc25bd189d8ed3b0
dim web <dmitrymail_k@mail.ru>
2021-11-06 02:32:46 +0300

    add setup file

--- Procfile
+++ Procfile
@@ -1,1 +1,2 @@
+release: python setup.py
 web: python ./server/main/english_learn/manage.py runserver 0.0.0.0:\$PORT
--- frontend/frontend_learn_english/src/utils/api.js
+++ frontend/frontend_learn_english/src/utils/api.js
@@ -1,9 +1,9 @@
 import axios from "axios";
 import store from "../store";
 // import router from "../router";
-
+import { config } from "../config/config";
 var apiServer = axios.create({
-  baseURL: "http://127.0.0.1:8000/",
+  baseURL: config.url,
 
   crossdomain: true,
 });
